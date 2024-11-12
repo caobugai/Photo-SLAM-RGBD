f6bb34bd47e19b5c603914b8fdb7f0fc3dcd612b
caobugai <czz332212807@gmail.com>
2024-11-12 12:28:42 +0800

    scripts

--- build.sh
+++ build.sh
@@ -39,6 +39,6 @@ echo "Building Photo-SLAM ..."
 cd ../..
 mkdir build
 cd build
-#cmake .. # add Torch_DIR and/or OpenCV_DIR definitions if needed, example:
-cmake .. -DTorch_DIR=/home/czz/czz/Library/libtorch2.0.1+cu11.8/libtorch/share/cmake/Torch
+cmake .. # add Torch_DIR and/or OpenCV_DIR definitions if needed, example:
+#cmake .. -DTorch_DIR=/home/czz/czz/Library/libtorch2.0.1+cu11.8/libtorch/share/cmake/Torch
 make -j8
